7e588bb327b69c3d9e2bd88f2d3a4eb5ec221209
Takashi Nakagawa <hawk4.n@gmail.com>
2017-07-21 21:12:02 +0900

    modify clone.py

--- clone.py
+++ clone.py
@@ -19,8 +19,15 @@ for line in lines:
   measurement = float(line[3])
   measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+  augmented_images.append(image)
+  augmented_measurements.append(measurement)
+  augmented_images.append(cv2.flip(image, 1))
+  augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Dropout, Activation
@@ -31,17 +38,18 @@ from keras.layers.pooling import MaxPooling2D
 
 model= Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Convolution2D(32, 3, 3))
-model.add(MaxPooling2D((2,2)))
+model.add(Convolution2D(6,5,5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5, activation="relu"))
+model.add(MaxPooling2D())
 model.add(Dropout(0.5))
 model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
         
