18b72a86587b436b7763eca324e35ffb18cd400d
Takashi Nakagawa <hawk4.n@gmail.com>
2017-07-21 21:57:40 +0900

    crop image

--- clone.py
+++ clone.py
@@ -4,33 +4,40 @@ import numpy as np
 
 lines = []
 with open('../data/driving_log.csv') as csvfile:
-  reader = csv.reader(csvfile)
-  for line in reader:
-    lines.append(line)
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+correction = 0.2
+
+def add_images_measurements(image_path, steering_delta, images, measurements):
+    filename = image_path.split('/')[-1]
+    current_path = '../data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement+ steering_delta)
 
 images = []
 measurements = []
 for line in lines:
-  source_path = line[0]
-  filename = source_path.split('/')[-1]
-  current_path = '../data/IMG/' + filename
-  image = cv2.imread(current_path)
-  images.append(image)
-  measurement = float(line[3])
-  measurements.append(measurement)
+    add_images_measurements(line[0],           0, images, measurements) # center
+    # add_images_measurements(line[1],  correction, images, measurements) # left
+    # add_images_measurements(line[2], -correction, images, measurements) # right
+
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
-  augmented_images.append(image)
-  augmented_measurements.append(measurement)
-  augmented_images.append(cv2.flip(image, 1))
-  augmented_measurements.append(measurement*-1.0)
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement*-1.0)
 
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout, Activation
+from keras.layers import Flatten, Dense, Lambda, Dropout, Activation, Cropping2D
 from keras.layers import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -38,6 +45,7 @@ from keras.layers.pooling import MaxPooling2D
 
 model= Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(6,5,5, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5, activation="relu"))
